ca7c93aa56d0be3f94f2642189af91e650531b26
Sujeet Kumar <60405591+Sujeet3099@users.noreply.github.com>
2021-05-07 19:06:12 +0530

    HRC Repo Confidential

---
+++ src/Components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import './header.css';
+import { TextField } from '@material-ui/core';
+import freeda from '../assets/Group 1206.svg';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <ArrowBackIcon />
+        <h2 style={{ color: '#5DAAE0', paddingLeft: '1rem' }}>Call Workboard</h2>
+      </div>
+      <div>
+        <span className="header__mid">AUTONOMOUS RECEIVABLES</span>
+      </div>
+      <div className="header__right">
+        <TextField variant="filled" />
+        <div className="header__btn">
+          <span className="freeda">Freeda</span>
+          <img src={freeda} alt="freeda-logo" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,63 @@
+// import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { Divider, IconButton, Typography, Drawer } from '@material-ui/core';
+import './navbar.css';
+import logo from '../logo.svg';
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const handleDrawerOpen = () => {
+    setOpen(!open);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar__top">
+          <IconButton onClick={handleDrawerOpen} style={{ color: '#fff' }}>
+            <MenuIcon style={{ fontSize: '2.75rem' }} />
+            <Divider style={{ color: '#fff' }} />
+          </IconButton>
+        </div>
+        <div className="navbar__bottom">
+          <img src={logo} alt="" className="navbar__user__img" />
+        </div>
+      </div>
+      <Drawer anchor="left" open={open} onClose={handleDrawerClose}>
+        <div className="navbar navbar__open">
+          <div>
+            <div className="navbar__top">
+              <IconButton onClick={handleDrawerOpen} style={{ color: '#fff' }}>
+                <MenuIcon style={{ fontSize: '2.75rem' }} />
+                <Typography style={{ paddingLeft: '1.25rem', fontSize: '2rem' }}>MENU</Typography>
+                <Divider style={{ color: '#fff' }} />
+              </IconButton>
+            </div>
+            <div className="navbar__mid" onClick={handleDrawerClose}>
+              <ArrowBackIcon />
+              <span style={{ paddingLeft: '1rem' }}>Switch Back to Enterprise UI</span>
+            </div>
+          </div>
+          <div className="navbar__bottom">
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <img src={logo} alt="" className="navbar__user__img" />
+              <span style={{ fontSize: '1.25rem' }}>User Name</span>
+            </div>
+            <button className="navbar__btn">Logout</button>
+          </div>
+        </div>
+      </Drawer>
+    </>
+  );
+};
+
+export default Navbar;
